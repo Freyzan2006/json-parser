5b1ff85c246e4df683aaf4d07c686d844f21711a
Freyzan2006 <freyzan2006@gmail.com>
2026-05-25 13:00:15 +0500

    init project

---
+++ bin/json_parser.dart
@@ -0,0 +1,5 @@
+import 'package:json_parser/json_parser.dart' as json_parser;
+
+void main(List<String> arguments) {
+  print('Hello world: ${json_parser.calculate()}!');
+}
---
+++ test/json_parser_test.dart
@@ -0,0 +1,8 @@
+import 'package:json_parser/json_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
